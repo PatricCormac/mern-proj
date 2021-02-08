d66e84d81d2802018da9232930fd43349aac19e9
PatricCormac <socolov1897@gmail.com>
2021-02-08 22:52:34 +0500

    configure routes, create pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { useRoutes } from "./routes";
+
 function App() {
+  const routes = useRoutes( false );
   return (
-    <div className="container">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className="container">
+        { routes }
+      </div>
+    </Router>
   );
 }
 
